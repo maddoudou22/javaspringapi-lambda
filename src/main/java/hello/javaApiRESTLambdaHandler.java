09431f0da4968eb50a0a8f40000db84260ab35e7
root <you@example.com>
2018-02-05 21:46:10 +0000

    dd

--- src/main/java/hello/javaApiRESTLambdaHandler.java
+++ src/main/java/hello/javaApiRESTLambdaHandler.java
@@ -21,7 +21,7 @@ public class javaApiRESTLambdaHandler {
     }
 
     public String myTestHandler(String name, Context context) {
-        return String.format("Hello v2 %s.", name);
+        return String.format("Hello versioning test %s.", name);
     }
 
     private boolean isPrime(long n) {
